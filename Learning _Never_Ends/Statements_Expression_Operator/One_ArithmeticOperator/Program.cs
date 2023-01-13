52ca1ba00e1dd62df150cf4f66d5f9e548a964e4
learn360Sujeet <learn360sujeet@gmail.com>
2023-01-13 19:51:07 +0530

    FULLY COMMANDED OVER RELATIONAL (REVISION)

---
+++ Learning _Never_Ends/Statements_Expression_Operator/One_ArithmeticOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿class Operator
+{
+    static void Main()
+    {
+        int a = 100;int b = 50, c, d ,e ,f ,g;
+        c = a + b;
+        d = a-b;
+        e = a * b;
+        f = a / b;
+        g = a % b;
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+        Console.WriteLine(e);
+        Console.WriteLine(f);
+        Console.WriteLine(g);
+
+
+    }
+   
+}
---
+++ Learning _Never_Ends/Statements_Expression_Operator/Two_RelationalOr_Comparision/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Numerics;
+
+int a = 20, b = 10;
+bool c=a== b;
+
+    Console.WriteLine(c);
+//operators in this .. ==,<,>,!=,<=,>=....
+//return TRUE,FALSE only
