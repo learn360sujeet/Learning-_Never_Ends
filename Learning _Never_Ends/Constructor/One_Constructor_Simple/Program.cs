4b429f1eb5ae74691d572bb938fa30d08658294a
learn360Sujeet <learn360sujeet@gmail.com>
2023-01-16 15:16:26 +0530

    Constructor

---
+++ Learning _Never_Ends/Constructor/One_Constructor_Simple/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    //Have the same name as the class name 
+    // will work  statements of constructor (inside only)after creating object of class 
+    //Default constructor 
+    public Program() 
+    {
+        Console.WriteLine("Constructor Concepts");
+    }
+
+    static void Main()
+    {
+        Program obj= new Program();
+    }
+     
+}
+  
---
+++ Learning _Never_Ends/Constructor/Two_Constructor_Parametrised/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+class Constructor
+{
+    string personName;
+    int age;
+    bool male;
+
+    public Constructor(string personName, int age, bool male)
+    {
+        this.personName = personName;
+        this.age = age;
+        this.male = male;
+    }
+
+    public string GetName()
+    {
+        return this.personName;
+    }
+    public int GetAge()
+    {
+        return this.age;
+    }
+    public bool GetMale()
+    {
+        return this.male;
+    }
+    static void Main()
+    {
+        Constructor constructor = new Constructor("Bhola", 25, true);
+
+        Console.WriteLine("your name is "+ constructor.GetName());
+        Console.WriteLine("your age is "+ constructor.GetAge()) ;
+        Console.WriteLine( "are you male"+constructor.GetMale()); 
+    }
+
+    
+}
